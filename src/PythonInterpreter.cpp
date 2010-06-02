69d520b62b4e8018cf872260d02f23cd55fedd18
Gary MacDonald <garymd@yahoo-inc.com>
2010-06-01 20:36:15 -0700

    checkpoint

--- src/PythonInterpreter.cpp
+++ src/PythonInterpreter.cpp
@@ -29,10 +29,10 @@
  *
  */
 
+#include "PythonHeaders.hh"
 #include "util/bpthread.hh"
 #include "util/bpsync.hh"
 #include "util/fileutils.hh"
-
 #include "PythonInterpreter.hh"
 #include "PythonWork.hh"
 #include "PythonUtils.hh"
@@ -40,68 +40,54 @@
 #include "DataMapping.hh"
 #include "BuiltIns.hh"
 #include "ServiceGlobals.hh"
-
-#include "PythonHeaders.hh"
-
 #include <list>
 
-static char ** s_argv;
+static char** s_argv;
 static int s_argc = 1;
 
-// the thread upon which the python interpreter will run
+// The thread upon which the python interpreter will run.
 static bp::thread::Thread s_pythonThread;
 static bp::sync::Mutex s_pythonLock;
 static bp::sync::Condition s_pythonCond;
 static bool s_running = false;
 static std::list<python::Work *> s_workQueue;
 
-static void * pythonThreadFunc(void * ctx)
-{
+static void*
+pythonThreadFunc(void* ctx) {
     std::string path((const char *) ctx);
-
     s_argv = (char **) calloc(2, sizeof(char *));
     s_argv[0] = "BrowserPlus Embedded Python";
     s_argv[1] = NULL;
-
-    // still unclear wether this is all right.  probably performed in
-    // a thread spawned at allocation time
+    // Still unclear wether this is all right.  Probably performed in
+    // a thread spawned at allocation time.
     Py_SetProgramName(s_argv[0]);
     Py_Initialize();
     PySys_SetArgv(s_argc, s_argv);
-#if 0
-    python_init();
-    int error = 0;
-    python_script("BrowserPlus Embedded Python");
     std::string pyPath = path + "/stdlib";
     std::string soPath = path + "/ext";
+#if 0
     python_incpush(pyPath.c_str());
     python_incpush(soPath.c_str());
-
+#endif // 0
     bp_load_builtins();
-
-    // include "browserplus.py" which cleans up the service authors
-    // definition semantics a bit
+#if 0
+    // Include "browserplus.py" which cleans up the service authors
+    // definition semantics a bit.
     rb_require("browserplus.py");
     rb_require("pathname");
-
-    // let's release the spawning thread
+#endif // 0
+    // Let's release the spawning thread.
     s_pythonLock.lock();
     s_running = true;
     s_pythonCond.signal();
     s_pythonLock.unlock();
-
-    // allocate a stack based container for managing anonymous
-    // object lifetime
+    // Allocate a stack based container for managing anonymous object lifetime.
     python::GCArray gcArray;
-#endif // 0
-
-#if 0
-    // now we'll block and wait for work
+    // Now we'll block and wait for work.
     while (s_running) {
-        // pop an item off the queue and process it,
-        // outside of the global data structure lock
-        python::Work * work = NULL;
-
+        // Pop an item off the queue and process it,
+        // outside of the global data structure lock.
+        python::Work* work = NULL;
         {
             s_pythonLock.lock();
             if (s_workQueue.size() > 0) {
@@ -110,125 +96,100 @@ static void * pythonThreadFunc(void * ctx)
             }
             s_pythonLock.unlock();
         }
-
         if (work == NULL) {
+#if 0
             PyObject* sleepyTime = rb_float_new(0.05);
-            // run the interpreter for a little bit to let background
-            // threads continue
-            // XXX: this absolutely sucks.  How do we kick this
-            //      python thread out of sleep when new work arrives?
-            //      signals?  something that we can do from another
-            //      thread safely is needed here.
+            // Run the interpreter for a little bit to let background
+            // threads continue.
+            // NEEDSWORK!!!  This absolutely sucks.  How do we kick this
+            // python thread out of sleep when new work arrives?
+            // signals?  Something that we can do from another
+            // thread safely is needed here.
             rb_funcall(rb_mKernel, rb_intern("sleep"), 1, sleepyTime);
+#endif // 0
         } else {
             if (work->m_type == python::Work::T_LoadService) {
-                // first lets update require path
+#if 0
+                // First lets update require path.
                 std::string serviceDir = file::dirname(work->sarg);
                 python_incpush(serviceDir.c_str());
-
-                // read python source file
+                // Read python source file.
                 std::string source = file::readFile(work->sarg);
-
                 if (source.empty()) {
                     work->m_error = true;
-                    work->m_verboseError.append("couldn't read: '" +
-                                                work->sarg + "'");
+                    work->m_verboseError.append("couldn't read: '" + work->sarg + "'");
                 } else {
                     int error = 0;
-                    (void) rb_eval_string_protect(source.c_str(), &error);
-
+                    (void)rb_eval_string_protect(source.c_str(), &error);
                     if (error) {
                         work->m_error = true;
                         work->m_verboseError = python::getLastError();
                     } else {
-                        // now it's time to pull out the global symbol
+                        // Now it's time to pull out the global symbol
                         // BrowserPlusEntryPointClass
                         // and call its to_service_description method
                         // and we'll get a python data structure we can
                         // traverse to discover the python interface
-                        work->m_desc =
-                            python::extractDefinition(work->m_verboseError);
+                        work->m_desc = python::extractDefinition(work->m_verboseError);
                         if (work->m_desc == NULL) {
                             work->m_error = true;
                         }
                     }
                 }
+#endif // 0
             }
-            else if (work->m_type == python::Work::T_AllocateInstance)
-            {
+            else if (work->m_type == python::Work::T_AllocateInstance) {
+#if 0
                 int error = 0;
                 PyObject* klass = rb_gv_get(python::BP_GLOBAL_DEF_SYM);
-
-                // initialize arguments
+                // Initialize arguments.
                 PyObject* initArgs = bpObjectToPython(work->m_obj, 0);
                 int takesArg = 0;
                 ID initialize = rb_intern("initialize");
-                if (rb_method_boundp(klass, initialize, 0))
-                {
-                    PyObject* initMeth =
-                        python::invokeFunction(
-                            klass, "instance_method", &error,
-                            1, ID2SYM(initialize));
-
+                if (rb_method_boundp(klass, initialize, 0)) {
+                    PyObject* initMeth = python::invokeFunction(klass, "instance_method", &error, 1, ID2SYM(initialize));
                     if (initMeth) {
-                        PyObject* arity = python::invokeFunction(
-                            initMeth, "arity", &error, 0);
+                        PyObject* arity = python::invokeFunction(initMeth, "arity", &error, 0);
                         if (NUM2INT(arity) >= 1) {
                             takesArg = 1;
                         }
                     }
                 }
-
-                work->m_instance =
-                    python::invokeFunction(klass, "new", &error, takesArg,
-                                         initArgs);
-
+                work->m_instance = python::invokeFunction(klass, "new", &error, takesArg, initArgs);
                 if (error) {
                     work->m_error = true;
                     work->m_verboseError = python::getLastError();
                 } else {
                     gcArray.Register(work->m_instance);
                 }
+#endif // 0
             }
-            else if (work->m_type == python::Work::T_InvokeMethod)
-            {
+            else if (work->m_type == python::Work::T_InvokeMethod) {
+#if 0
                 int error = 0;
                 PyObject* tid = rb_uint_new(work->m_tid);
-                PyObject* trans = rb_class_new_instance(1, &tid,
-                                                    bp_py_cTransaction);
-
-                g_bpCoreFunctions->log(
-                    BP_DEBUG, "executing func '%s'",
-                    work->sarg.c_str());
-
-                python::invokeFunction(
-                    work->m_instance, work->sarg.c_str(),
-                    &error, 2, trans,
-                    bpObjectToPython(work->m_obj, work->m_tid));
-
+                PyObject* trans = rb_class_new_instance(1, &tid, bp_py_cTransaction);
+                g_bpCoreFunctions->log(BP_DEBUG, "executing func '%s'", work->sarg.c_str());
+                python::invokeFunction(work->m_instance, work->sarg.c_str(), &error, 2, trans, bpObjectToPython(work->m_obj, work->m_tid));
                 if (error) {
-                    g_bpCoreFunctions->postError(
-                        work->m_tid, "python.evalError",
-                        python::getLastError().c_str());
+                    g_bpCoreFunctions->postError(work->m_tid, "python.evalError", python::getLastError().c_str());
                 }
-
                 if (work->m_obj) {
                     delete work->m_obj;
                     work->m_obj = NULL;
                 }
-            } else if (work->m_type == python::Work::T_ReleaseInstance) {
-                if (rb_method_boundp(CLASS_OF(work->m_instance),
-                                     rb_intern("destroy"), 1))
-                {
-                    (void) python::invokeFunction(work->m_instance,
-                                                "destroy", &error, 0);
-                }
-
-                gcArray.Unregister(work->m_instance);
+#endif // 0
             }
-
-            // presence of syncLock indicates synchronous operation
-            // (client freed)
+            else if (work->m_type == python::Work::T_ReleaseInstance) {
+#if 0
+                if (rb_method_boundp(CLASS_OF(work->m_instance), rb_intern("destroy"), 1)) {
+                    (void) python::invokeFunction(work->m_instance, "destroy", &error, 0);
+                }
+                gcArray.Unregister(work->m_instance);
+#endif // 0
+            }
+            // Presence of syncLock indicates synchronous operation
+            // (client freed).
             if (work->m_syncLock != NULL) {
                 work->m_syncLock->lock();
                 work->m_done = true;
@@ -239,20 +200,17 @@ static void * pythonThreadFunc(void * ctx)
             }
         }
     }
-#endif // 0
-
-    // now we'll block and wait for work
+    // Now we'll block and wait for work.
     s_pythonLock.unlock();
-
     return NULL;
 }
 
-// a utility routine to synchronously run "work" on the python interpreter
-// thread, blocking until the work is complete
-static void runWorkSync(python::Work * work)
-{
-    // *note* in this case work will not be deleted by this function nor
-    // the interpreter thread
+// A utility routine to synchronously run "work" on the python interpreter
+// thread, blocking until the work is complete.
+static void
+runWorkSync(python::Work* work) {
+    // *NOTE* In this case work will not be deleted by this function nor
+    // the interpreter thread.
     s_pythonLock.lock();
     s_workQueue.push_back(work);
     s_pythonCond.signal();
@@ -260,35 +218,38 @@ static void runWorkSync(python::Work * work)
     work->m_syncCond = new bp::sync::Condition;
     work->m_syncLock->lock();
     s_pythonLock.unlock();
-    do { work->m_syncCond->wait(work->m_syncLock); } while (!work->m_done);
+    do {
+        work->m_syncCond->wait(work->m_syncLock);
+    } while (!work->m_done);
     work->m_syncLock->unlock();
 }
 
-// a utility routine to asynchronously run "work" on the python interpreter
-// thread. work is dynamically allocated by the caller and will
-// be freed by the interpreter thread
-static void runWorkASync(python::Work * work)
-{
+// A utility routine to asynchronously run "work" on the python interpreter
+// thread.  Work is dynamically allocated by the caller and will
+// be freed by the interpreter thread.
+static void runWorkASync(python::Work* work) {
     s_pythonLock.lock();
     s_workQueue.push_back(work);
     s_pythonCond.signal();
     s_pythonLock.unlock();
 }
 
-void python::initialize(const std::string & path)
-{
+void
+python::initialize(const std::string& path) {
     if (!s_running) {
         s_pythonLock.lock();
-        if (s_pythonThread.run(pythonThreadFunc, (void *) path.c_str())) {
-            while (!s_running) s_pythonCond.wait(&s_pythonLock);
+        if (s_pythonThread.run(pythonThreadFunc, (void*)path.c_str())) {
+            while (!s_running) {
+                s_pythonCond.wait(&s_pythonLock);
+            }
         }
         s_pythonLock.unlock();
     }
 }
 
-void python::shutdown(void)
-{
-    // stop the python thread!
+void
+python::shutdown(void) {
+    // Stop the python thread!
     if (s_running) {
         s_pythonLock.lock();
         s_running = false;
@@ -299,76 +260,51 @@ void python::shutdown(void)
     Py_Finalize();
 }
 
-bp::service::Description *
-python::loadPythonService(const std::string & pathToPythonFile,
-                      std::string & oError)
-{
-#if 0
+bp::service::Description*
+python::loadPythonService(const std::string& pathToPythonFile, std::string& oError) {
     python::Work work(python::Work::T_LoadService);
     work.sarg.append(pathToPythonFile);
-
     runWorkSync(&work);
-
     if (work.m_error) {
         oError = work.m_verboseError;
     }
-
-    return work.m_desc; // if non-null, caller owns
-#else // 0
-    return NULL;
-#endif // 0
+    // If non-null, caller owns.
+    return work.m_desc;
 }
 
-void *
-python::allocateInstance(const bp::Map * context)
-{
-#if 0
+void*
+python::allocateInstance(const bp::Map* context) {
     python::Work work(python::Work::T_AllocateInstance);
     work.m_obj = context;
-
     runWorkSync(&work);
-
     if (work.m_error) {
-        g_bpCoreFunctions->log(
-            BP_ERROR,
-            "failed to allocate instance: %s",
-            work.m_verboseError.c_str());
+        g_bpCoreFunctions->log(BP_ERROR, "failed to allocate instance: %s", work.m_verboseError.c_str());
         return NULL;
     }
-
-    return (void *) work.m_instance; // if non-null, caller owns
-#else // 0
-    return NULL;
-#endif // 0
+    // If non-null, caller owns.
+    return (void*)work.m_instance;
 }
 
-
 void
-python::invoke(void * instance, const char * funcName,
-             unsigned int tid, bp::Map * arguments)
-{
-#if 0
-    // set up a dynamically allocated structure with information about
-    // the method invocation
-    python::Work * work = new python::Work(python::Work::T_InvokeMethod);
-    work->m_instance = (PyObject*) instance;
+python::invoke(void* instance, const char* funcName, unsigned int tid, bp::Map* arguments) {
+    // Set up a dynamically allocated structure with information about the method invocation.
+    python::Work* work = new python::Work(python::Work::T_InvokeMethod);
+    work->m_instance = (PyObject*)instance;
     work->sarg.append(funcName);
     work->m_tid = tid;
-    if (arguments) { work->m_obj = arguments->clone(); }
-    else work->m_obj = NULL;
-
-    // asynchronously run this work, not waiting around for the results
+    if (arguments) {
+        work->m_obj = arguments->clone();
+    }
+    else {
+        work->m_obj = NULL;
+    }
+    // Asynchronously run this work, not waiting around for the results.
     runWorkASync(work);
-#endif // 0
 }
 
-
 void
-python::destroyInstance(void * instance)
-{
-#if 0
+python::destroyInstance(void* instance) {
     python::Work work(python::Work::T_ReleaseInstance);
-    work.m_instance = (PyObject*) instance;
+    work.m_instance = (PyObject*)instance;
     runWorkSync(&work);
-#endif // 0
 }
--- src/service.cpp
+++ src/service.cpp
@@ -28,25 +28,18 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "ServiceGlobals.hh"
-
-// header files from the bp-service-tools project, which makes it
-// easier to deal in types that one may transmit across the service
-// boundary
-#include "bptypeutil.hh"
-
-// an abstraction around python
 #include "PythonInterpreter.hh"
 #include "PythonHeaders.hh"
-
-#if 0
+#include "ServiceGlobals.hh"
+// Header files from the bp-service-tools project, which makes it
+// easier to deal in types that one may transmit across the service
+// boundary.
+#include "bptypeutil.hh"
 #include <map>
 #include <string>
 #include <iostream>
-
 #include <stdio.h>
 #include <stdlib.h>
-#endif // 0
 
 // a description of this service.
 BPCoreletDefinition s_pythonInterpreterDef = {
@@ -57,155 +50,113 @@ BPCoreletDefinition s_pythonInterpreterDef = {
     NULL
 };
 
-const BPCFunctionTable * g_bpCoreFunctions;
+const BPCFunctionTable* g_bpCoreFunctions;
 
-const BPCoreletDefinition *
-BPPInitialize(const BPCFunctionTable * bpCoreFunctions,
-              const BPElement * parameterMap)
-{
-    // the name of the python script and path can be extracted from the
-    // parameter map
-    bp::Object * obj = bp::Object::build(parameterMap);
-
-    // first get the path
+const BPCoreletDefinition*
+BPPInitialize(const BPCFunctionTable* bpCoreFunctions, const BPElement* parameterMap) {
+    // The name of the python script and path can be extracted from the parameter map.
+    bp::Object* obj = bp::Object::build(parameterMap);
+    // First get the path.
     if (!obj->has("CoreletDirectory", BPTString)) {
         delete obj;
         return NULL;
     }
-
-    std::string path(((bp::String *) obj->get("CoreletDirectory"))->value());
-
+    std::string path(((bp::String*)obj->get("CoreletDirectory"))->value());
     g_bpCoreFunctions = bpCoreFunctions;
-
     delete obj;
-
-    // this will go in the BrowserPlusCore log file at info level.  nice.
-
-    g_bpCoreFunctions->log(
-        BP_INFO,
-        "initializing python interpreter with service path: %s",
-        path.c_str());
-
-    // now let's initialize the python Interpreter
-    (void) python::initialize(path);
-
+    // This will go in the BrowserPlusCore log file at info level.  Nice.
+    g_bpCoreFunctions->log(BP_INFO, "initializing python interpreter with service path: %s", path.c_str());
+    // Now let's initialize the python Interpreter.
+    (void)python::initialize(path);
     return &s_pythonInterpreterDef;
 }
 
 void
-BPPShutdown(void)
-{
+BPPShutdown(void) {
     python::shutdown();
 }
 
-#if 0
 int
-BPPAllocate(void ** instance, unsigned int,
-            const BPElement * contextMap)
-{
-    bp::Object * obj = bp::Object::build(contextMap);
-    *instance = python::allocateInstance(dynamic_cast<bp::Map *>(obj));
-    if (obj) delete obj;
-
-    // XXX failure case?
+BPPAllocate(void** instance, unsigned int, const BPElement* contextMap) {
+    bp::Object* obj = bp::Object::build(contextMap);
+    *instance = python::allocateInstance(dynamic_cast<bp::Map*>(obj));
+    if (obj) {
+        delete obj;
+    }
+    // NEEDSWORK!!! Failure case?
     return 0;
 }
-#endif // 0
 
-#if 0
 void
-BPPDestroy(void * instance)
-{
+BPPDestroy(void* instance) {
     python::destroyInstance(instance);
 }
-#endif // 0
 
-#if 0
 void
-BPPInvoke(void * instance, const char * funcName,
-          unsigned int tid, const BPElement * elem)
-{
-    bp::Object * obj = bp::Object::build(elem);
-    python::invoke(instance, funcName, tid, dynamic_cast<bp::Map *>(obj));
-    if (obj) delete obj;
+BPPInvoke(void* instance, const char* funcName, unsigned int tid, const BPElement* elem) {
+    bp::Object* obj = bp::Object::build(elem);
+    python::invoke(instance, funcName, tid, dynamic_cast<bp::Map*>(obj));
+    if (obj) {
+        delete obj;
+    }
 }
-#endif // 0
 
-#if 0
-// file scoped memory representation of the services interface.
-static bp::service::Description * s_desc = NULL;
-#endif // 0
+// File scoped memory representation of the services interface.
+static bp::service::Description* s_desc = NULL;
 
-#if 0
 #ifdef WIN32
 #define PATHSEP "\\"
 #else
 #define PATHSEP "/"
 #endif
-#endif // 0
 
-#if 0
-const BPCoreletDefinition *
-BPPAttach(unsigned int attachID, const BPElement * paramMap)
-{
-    const BPCoreletDefinition * def = NULL;
-
-    if (def != NULL) return def;
-
-    // the name of the python script and path can be extracted from the
-    // parameter map
-    bp::Object * obj = bp::Object::build(paramMap);
-
-    // first get the path
+const BPCoreletDefinition*
+BPPAttach(unsigned int attachID, const BPElement* paramMap) {
+    const BPCoreletDefinition* def = NULL;
+    if (def != NULL) {
+        return def;
+    }
+    // The name of the python script and path can be extracted from the parameter map.
+    bp::Object* obj = bp::Object::build(paramMap);
+    // First get the path.
     if (!obj->has("CoreletDirectory", BPTString)) {
         delete obj;
         return NULL;
     }
     std::string path;
-    path.append(((bp::Path *) obj->get("CoreletDirectory"))->value());
-
-    // now get the script name
+    path.append(((bp::Path*)obj->get("CoreletDirectory"))->value());
+    // Now get the script name.
     if (!obj->has("Parameters", BPTMap)) {
         delete obj;
         return NULL;
     }
-
-    bp::Map * params = (bp::Map *) obj->get("Parameters");
-
+    bp::Map* params = (bp::Map*)obj->get("Parameters");
     if (!params->has("ScriptFile", BPTString)) {
         delete obj;
         return NULL;
     }
-
     path.append(PATHSEP);
-    path.append(((bp::Path *) params->get("ScriptFile"))->value());
-
+    path.append(((bp::Path*)params->get("ScriptFile"))->value());
     std::string error;
     s_desc = python::loadPythonService(path, error);
-
     if (s_desc == NULL) {
-        g_bpCoreFunctions->log(
-            BP_ERROR, "error loading python service: %s",
-            error.c_str());
-
+        g_bpCoreFunctions->log(BP_ERROR, "error loading python service: %s", error.c_str());
         return NULL;
     }
-
     return (def = s_desc->toBPCoreletDefinition());
 }
-#endif // 0
 
-#if 0
 void
-BPPDetach(unsigned int attachID)
-{
-    if (s_desc) delete s_desc;
+BPPDetach(unsigned int attachID) {
+    if (s_desc) {
+        delete s_desc;
+    }
     s_desc = NULL;
 }
-#endif // 0
 
-#if 0
-/** and finally, declare the entry point to the service */
+/**
+ * And finally, declare the entry point to the service.
+ */
 BPPFunctionTable funcTable = {
     BPP_CORELET_API_VERSION,
     BPPInitialize,
@@ -216,12 +167,8 @@ BPPFunctionTable funcTable = {
     BPPAttach,
     BPPDetach
 };
-#endif // 0
 
-#if 0
-const BPPFunctionTable *
-BPPGetEntryPoints(void)
-{
+const BPPFunctionTable*
+BPPGetEntryPoints(void) {
     return &funcTable;
 }
-#endif // 0
