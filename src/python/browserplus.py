06613b3000c3860657a70704a2e2809e6bf72ab3
Gary MacDonald <garymd@yahoo-inc.com>
2010-06-17 22:54:02 -0700

    service description fixes

--- src/python/browserplus.py
+++ src/python/browserplus.py
@@ -1,3 +1,5 @@
+import re
+
 # divide a list into chunks
 def div_list(a, len):
     new_list = []
@@ -7,7 +9,6 @@ def div_list(a, len):
         sub_list = new_list[i / len]
         sub_list.append(x)
         new_list[i / len] = sub_list
-    end
     return new_list
 
 # python's chomp sucks
@@ -20,39 +21,53 @@ def chomp2(s):
 
 BrowserPlusEntryPointClass = None
 
-def bp_version(service_class, version_string):
-    service_class.bp_version = chomp2(version_string)
+def bp_version(version_string):
+    def decorator(service_class):
+        global BrowserPlusEntryPointClass
+        if (BrowserPlusEntryPointClass == None):
+            BrowserPlusEntryPointClass = service_class
+        elif (BrowserPlusEntryPointClass != service_class):
+            raise RuntimeError("multiple entry point classes detected!  you may only have a single \"entry point class\" which uses bp_version functions")
+        service_class.bp_version = chomp2(version_string)
+        return service_class
+    return decorator
 
-def bp_doc(service_class, service_method, doc_string):
-    global BrowserPlusEntryPointClass
-    if (BrowserPlusEntryPointClass == None):
-        BrowserPlusEntryPointClass = service_class
-    elif (BrowserPlusEntryPointClass != service_class):
-        raise RuntimeError("multiple entry point classes detected!  you may only have a single \"entry point class\" which uses bp_doc functions")
-    if (service_class.bp_doc == None):
-        service_class.bp_doc = {}
-    if (desc == None):
-        desc = m
-    # parse it out
-    ar = re.split(desc, '/^\s+([\[<])(\w+:\s*\w+)[\]>]\s+/')
-    doc = ar[0]
-    m = list(ar[1:])
-    m = div_list(m, 3)
-    newlist = []
-    for x, i in enumerate(m):
-        bracket = x[0]
-        arg = x[1]
-        adoc = x[2]
-        ar2 = re.split(arg, '/:\s*/')
-        aname = ''
-        atype = ''
-        if (len(ar2) > 2):
-            aname = ar2[0]
-            atype = ar2[1]
-        elif (len(ar2) > 1):
-            aname = ar2[0]
-        newlist[i] = {'name': aname, 'type': atype, 'documentation': chomp2(adoc), 'required': (bracket == "<")}
-    service_class.bp_doc[None] = [doc.strip(), newlist]
+def bp_doc(service_method, doc_string):
+    def decorator(service_class):
+        global BrowserPlusEntryPointClass
+        if (BrowserPlusEntryPointClass == None):
+            BrowserPlusEntryPointClass = service_class
+        elif (BrowserPlusEntryPointClass != service_class):
+            raise RuntimeError("multiple entry point classes detected!  you may only have a single \"entry point class\" which uses bp_doc functions")
+        if (not hasattr(service_class, "bp_doc")):
+            service_class.bp_doc = {}
+        if (service_class.bp_doc == None):
+            service_class.bp_doc = {}
+        # parse it out
+        real_doc_string = doc_string
+        if (real_doc_string == None or real_doc_string == ""):
+            real_doc_string = service_method
+        ar = re.split(real_doc_string, '/^\s+([\[<])(\w+:\s*\w+)[\]>]\s+/')
+        doc = ar[0]
+        m = list(ar[1:])
+        m = div_list(m, 3)
+        newlist = []
+        for x, i in enumerate(m):
+            bracket = x[0]
+            arg = x[1]
+            adoc = x[2]
+            ar2 = re.split(arg, '/:\s*/')
+            aname = ''
+            atype = ''
+            if (len(ar2) > 2):
+                aname = ar2[0]
+                atype = ar2[1]
+            elif (len(ar2) > 1):
+                aname = ar2[0]
+            newlist[i] = {'name': aname, 'type': atype, 'documentation': chomp2(adoc), 'required': (bracket == "<")}
+            service_class.bp_doc[None] = [doc.strip(), newlist]
+        return service_class
+    return decorator
 
 def bp_to_service_description(service_class):
     l = service_class.bp_doc[None]
--- tests/BadType/bad_type.py
+++ tests/BadType/bad_type.py
@@ -1,13 +1,14 @@
 #
 # A simple service with a bogus type in its description
 #
+import browserplus
 
-@bp_version(BadType, "1.0.0")
-@bp_doc(BadType, "A hello world test service for BrowserPlus.")
+@browserplus.bp_version("1.0.0")
+@browserplus.bp_doc("A hello world test service for BrowserPlus.", None)
 class BadType:
-    @bp_doc(BadType, ":hello", "return the string, 'hello world'.  original, eh?\n\
-    <who: string> who to say hello to.\n\
-    [cb: bogus] a callback to invoke (with a bogus type name)")
+#    @browserplus.bp_doc(":hello", "return the string, 'hello world'.  original, eh?\n\
+#    <who: string> who to say hello to.\n\
+#    [cb: bogus] a callback to invoke (with a bogus type name)")
     def hello(trans, args):
         if cb in args:
             args[cb].invoke("Hi there #{args[:who]}")
