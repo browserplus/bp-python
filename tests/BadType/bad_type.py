109da7d58b67aebd58e1090d7cadc49a79dabfc8
Gary MacDonald <garymd@yahoo-inc.com>
2010-06-17 20:47:43 -0700

    dev fix

--- tests/BadType/bad_type.py
+++ tests/BadType/bad_type.py
@@ -5,9 +5,9 @@
 @bp_version(BadType, "1.0.0")
 @bp_doc(BadType, "A hello world test service for BrowserPlus.")
 class BadType:
-    @bp_doc(BadType, ":hello", "return the string, 'hello world'.  original, eh?
-                                <who: string> who to say hello to.
-                                [cb: bogus] a callback to invoke (with a bogus type name)"
+    @bp_doc(BadType, ":hello", "return the string, 'hello world'.  original, eh?\n\
+<who: string> who to say hello to.\n\
+[cb: bogus] a callback to invoke (with a bogus type name)"
     def hello(trans, args):
         args[:cb].invoke("Hi there #{args[:who]}") if args.has_key? :cb
         trans.complete("hello #{args[:who]}")
