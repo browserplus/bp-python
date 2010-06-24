d6d183abffb65f1efed87bd28ec823bdbcffb8c7
Gary MacDonald <garymd@yahoo-inc.com>
2010-06-24 13:48:02 -0700

    dylib fixup was using wrong name

--- src/libpython_fixup.sh
+++ src/libpython_fixup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 echo working on $1
-AFTER='@loader_path/libpython_bp.dylib'
+AFTER='@loader_path/libpython2.6_bp.dylib'
 BEFORE=`otool -LX $1 | grep libpython | sed -E -e 's/^[^/]*\//\//g' -e 's/dylib .*$/dylib/'`
-install_name_tool -id libpython_bp.dylib -change "${BEFORE}" "${AFTER}" $1
+install_name_tool -id libpython2.6_bp.dylib -change "${BEFORE}" "${AFTER}" $1
