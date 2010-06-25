9e1350bf243e415f04225a093168bb305e92c1de
Gary MacDonald <garymd@yahoo-inc.com>
2010-06-24 18:34:40 -0700

    fix linking to pylibs and add non-functional basicservice test

---
+++ tests/BasicService/hello_world.py
@@ -0,0 +1,27 @@
+#
+# A rough sketch of how a service under python engine 1.x.x may look.
+# 
+import browserplus
+
+@browserplus.bp_version("1.0.0")
+@browserplus.bp_doc("A hello world test service for BrowserPlus.")
+@browserplus.bp_doc("hello:", "return the string, 'hello world'.  original, eh?\n\
+                               <who: string> who to say hello to.\n\
+                               [cb: bogus] a callback to invoke (with a bogus type name)")
+@browserplus.bp_doc("syntax:", "A function which takes no args and has a syntax error")
+class HelloWorld:
+    def initialize(args):
+        #require 'pp'
+        print "init called!  w00t"
+        #pp args
+
+    def hello(trans, args):
+        if cb in args:
+            args[cb].invoke("Hi there #{args[:who]}")
+        trans.complete("hello #{args[:who]}")
+
+    def syntax(trans, args):
+        foo
+
+    def destroy:
+        print "destroy called!  thanks for calling my destructor, BrowserPlus"
