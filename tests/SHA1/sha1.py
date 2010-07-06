0fe50867a2374942dd2595b9a56216dc6f596020
Gary MacDonald <garymd@yahoo-inc.com>
2010-07-06 11:31:35 -0700

    sha1 test

--- tests/SHA1/sha1.py
+++ tests/SHA1/sha1.py
@@ -1,11 +1,13 @@
 # Toy crypto service for testing built-in python libraries
 
 import browserplus
-#require 'digest/sha1'
+import hashlib
 
 @browserplus.bp_version("1.0.0")
 @browserplus.bp_doc("A simple if digest/ built in extensions.")
 @browserplus.bp_doc("sha1", "calc the sha1 of \"hello world\"")
 class SHA1:
     def sha1(self, trans, args):
-        #trans.complete(Digest::SHA1.hexdigest("hello world"))
+        m = hashlib.sha1()
+        m.update("hello world")
+        trans.complete(m.digest())
