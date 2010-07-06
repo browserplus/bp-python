3dd194d060f6c4b6715aa52fae4f786ed8767040
Gary MacDonald <garymd@yahoo-inc.com>
2010-07-05 17:04:50 -0700

    continued test dev

--- tests/SHA1/sha1.py
+++ tests/SHA1/sha1.py
@@ -1,13 +1,10 @@
 # Toy crypto service for testing built-in ruby extensions
+import browserplus
+#require 'digest/sha1'
 
-require 'digest/sha1'
-
-class SHA1
-  bp_version "1.0.0"
-  bp_doc "A simple if digest/ built in extensions"
-
-  def sha1(trans, args)
-    trans.complete(Digest::SHA1.hexdigest("hello world"))
-  end
-  bp_doc :sha1, "calc the sha1 of \"hello world\""
-end
+@browserplus.bp_version("1.0.0")
+@browserplus.bp_doc("A simple if digest/ built in extensions.")
+@browserplus.bp_doc("sha1", "calc the sha1 of \"hello world\"")
+class SHA1:
+    def sha1(self, trans, args):
+        #trans.complete(Digest::SHA1.hexdigest("hello world"))
