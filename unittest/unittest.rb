c477b6f5f2331bdacb285ec9896bfed8525105b7
Gary MacDonald <garymd@yahoo-inc.com>
2010-10-07 10:59:01 -0700

    updated service-testing and update unittests

--- unittest/unittest.rb
+++ unittest/unittest.rb
@@ -23,7 +23,7 @@ class TestPython < Test::Unit::TestCase
   end
 
   def test_file_checksum
-    BrowserPlus.run(File.join(@cwd, "FileChecksum"), @interpService) { |s|
+    BrowserPlus.runProvider(File.join(@cwd, "FileChecksum"), @interpService) { |s|
       curDir = File.dirname(__FILE__)
       textfile_path = File.expand_path(File.join(curDir, "services.txt"))
       # XXX: service runner needs to grow up here.
@@ -34,7 +34,7 @@ class TestPython < Test::Unit::TestCase
   end
 
   def test_basic_service
-    BrowserPlus.run(File.join(@cwd, "BasicService"), @interpService) { |s|
+    BrowserPlus.runProvider(File.join(@cwd, "BasicService"), @interpService) { |s|
       require 'pp'
       r = s.hello({:who => 'lloyd'}) { |o|
         assert_equal o['callback'], 1
@@ -49,7 +49,7 @@ class TestPython < Test::Unit::TestCase
 
   # basic test of built in extensions
   def test_sha1
-    BrowserPlus.run(File.join(@cwd, "SHA1"), @interpService) { |s|
+    BrowserPlus.runProvider(File.join(@cwd, "SHA1"), @interpService) { |s|
       require 'digest/sha1'
       assert_equal s.sha1, Digest::SHA1.hexdigest("hello world")
     }
@@ -59,7 +59,7 @@ class TestPython < Test::Unit::TestCase
   # slightly deeper test of built in extensions, generate
   # some keypairs!
 #  def test_crypto
-#    BrowserPlus.run("Crypto", @interpService) { |s|
+#    BrowserPlus.runProvider("Crypto", @interpService) { |s|
 #      assert_equal 342, s.generate.length
 #      assert_equal 342, s.generate.length
 #      assert_equal 342, s.generate.length
@@ -72,7 +72,7 @@ class TestPython < Test::Unit::TestCase
   # A junk ruby file
   def test_syntax_error
     assert_raise(RuntimeError) do 
-      BrowserPlus.run(File.join(@cwd, "SyntaxError"), @interpService) { |s| }
+      BrowserPlus.runProvider(File.join(@cwd, "SyntaxError"), @interpService) { |s| }
     end
   end
 
@@ -80,14 +80,14 @@ class TestPython < Test::Unit::TestCase
   # test that there's verbose and useful information in the log output)
   def test_bad_type
     assert_raise(RuntimeError) do 
-      BrowserPlus.run(File.join(@cwd, "BadType"), @interpService) { |s| }
+      BrowserPlus.runProvider(File.join(@cwd, "BadType"), @interpService) { |s| }
     end
   end
 
   # A bad type defined within the ruby file - (NOTE: really wish we could
   # test that there's verbose and useful information in the log output)
   def test_require_stmt
-    BrowserPlus.run(File.join(@cwd, "RequireTest"), @interpService) { |s|
+    BrowserPlus.runProvider(File.join(@cwd, "RequireTest"), @interpService) { |s|
       assert_equal s.yo, "a string"
     }
   end
